41775a6b060b7eed8fab4ed916a1430b891c4dba
Rajan Patel <rajan.n.patel.97@gmail.com>
2018-02-13 18:41:41 +0000

    Add files via upload

---
+++ Final MicroPython Code/newmain.py
@@ -0,0 +1,47 @@
+import ujson
+import time
+import network
+import machine
+import micropython
+import time
+from umqtt.simple import MQTTClient
+
+#Connect to WIFI
+ap_if = network.WLAN(network.AP_IF)
+ap_if.active(False)
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+sta_if.connect('EEERover', 'exhibition')
+while (not sta_if.isconnected()):
+    pass
+print('connected')
+
+
+
+# Publish test messages e.g. with:
+# mosquitto_pub -t foo_topic -m hello
+
+# Received messages from subscriptions will be delivered to this callback
+def sub_cb(topic, msg):
+    print((topic, msg))
+
+def main(server="192.168.0.10"):
+    c = MQTTClient("umqtt_client", server)
+    c.set_callback(sub_cb)
+    c.connect()
+    c.subscribe(b"esys/KANYE2020")
+    while True:
+        if True:
+            # Blocking wait for message
+            c.wait_msg()
+        else:
+            # Non-blocking wait for message
+            c.check_msg()
+            # Then need to sleep to avoid 100% CPU usage (in a real
+            # app other useful actions would be performed instead)
+            time.sleep(1)
+
+    c.disconnect()
+
+if __name__ == "__main__":
+    main()
